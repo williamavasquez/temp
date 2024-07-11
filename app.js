73695ad8f8a8daa885610f59b7d25d8853197114
William A Vasquez <william.a.vasquez@gmail.com>
2024-07-10 21:47:22 -0700

    initial filed

---
+++ app.js
@@ -0,0 +1,10 @@
+import { FFF } from "./newFolderComponent/someFunction.mjs";
+
+function test() {
+  console.log("Function is working normally! ");
+  console.log("import is working fine -->");
+
+  console.log(FFF());
+}
+
+test();
---
+++ newFolderComponent/someFunction.mjs
@@ -0,0 +1,5 @@
+function FFF() {
+  return "some random text to show this is being imported";
+}
+
+export { FFF };
